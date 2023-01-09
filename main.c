6a4f053a5c03cad0f667c816420d82a4462ca02f
Abel Tiago Pereira <apereira@c2r8s2.42lisboa.com>
2023-01-09 18:02:40 +0000

    added some new stuff

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: apereira <apereira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 14:07:59 by apereira          #+#    #+#             */
-/*   Updated: 2023/01/09 16:49:05 by apereira         ###   ########.fr       */
+/*   Updated: 2023/01/09 17:58:29 by apereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ int	main(int argc, char **argv)
 	t_key	game;
 	t_map	*map;
 	int		fd;
-
 	if (!(argc == 2))
 		return (0);
 	fd = open(argv[1], O_RDONLY);
@@ -28,7 +27,8 @@ int	main(int argc, char **argv)
 	map->large = ft_strlen(map->matriz[0]);
 	vars_init(&game, map);
 	open_wdw(&game);
-	mlx_pixel_put(game.mlx, game.wdw, 100, 100, 255);
+	game.img = mlx_xpm_file_to_image(game.mlx, "grass.xpm", NULL, NULL);
+	mlx_put_image_to_window(game.mlx, game.wdw, game.img, 0, 0);
 	mlx_hook(game.wdw, 2, 1L << 0, key_press, &game);
 	mlx_hook(game.wdw, 17, 1L << 17, destroy_wdw, &game);
 	mlx_loop(game.mlx);
