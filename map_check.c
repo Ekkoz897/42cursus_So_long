5cf3701485d4fde840684b358190add96e346adb
Ekkoz897 <abelix897@gmail.com>
2023-01-13 00:21:31 +0000

    pc to VM

--- map_check.c
+++ map_check.c
@@ -15,8 +15,6 @@
 // passa o mapa para uma matris e conta as linhas (altura)
 char	**map_cpy(int fd, char *argv, t_game	*game)
 {
-	char	*str;
-
 	while (get_next_line(fd))
 		game->large++;
 	close(fd);
@@ -24,8 +22,7 @@ char	**map_cpy(int fd, char *argv, t_game	*game)
 	fd = open(argv, O_RDONLY);
 	while (game->large > 0)
 	{
-		str = get_next_line(fd);
-		game->matriz[game->tall] = str;
+		game->matriz[game->tall] = get_next_line(fd);
 		game->tall++;
 		game->large--;
 	}
@@ -74,8 +71,8 @@ int	check_cpe(char **matriz, t_game *game)
 			else if (matriz[game->i][game->j] == 'P')
 			{
 				game->p++;
-				game->p_x = game->i;
-				game->p_y = game->j;
+				game->p_x = game->j;
+				game->p_y = game->i;
 			}
 			else if (matriz[game->i][game->j] == 'E')
 				game->e++;
@@ -85,7 +82,30 @@ int	check_cpe(char **matriz, t_game *game)
 				return (0);
 		}
 	}
-	if (!game->food || game->p != 1 || game->e != 1)
+	if (game->food = 0 || game->p != 1 || game->e != 1)
 		return (0);
-	return (1);
+	if (check_path(game->p_x, game->p_y, game));
+		return (1);
+}
+
+int	check_path(int x, int y, t_game *game)
+{
+	if (game->matriz[i][j] == '0' || game->matriz[i][j] == 'P' || game->matriz[i][j] == 'C' || game->matriz[i][j] == 'E')
+	{
+		if (game->matriz[x][y] == '0')
+			game->matriz[x][y] = '-';
+		if (game->matriz[x][y] == 'C')
+			game->matriz[x][y] = 'c';
+		if (game->matriz[x][y] == 'E')
+			game->matriz[x][y] = 'e';
+		if (check_path(x, y - 1, game))
+			return (1);
+		if (check_path(x + 1, y, game))
+			return (1);
+		if (check_path(x, y + 1, game))
+			return (1);
+		if (check_path(x - 1, y, game))
+			return (1);
+	}
+	return (0);
 }
