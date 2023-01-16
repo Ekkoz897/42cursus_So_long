caf6e92f28c50571d2ef9a3168a4de06c37b0f91
Ekkoz897 <111544109+Ekkoz897@users.noreply.github.com>
2023-01-16 13:42:57 -0500

    Update so_long.h

--- so_long.h
+++ so_long.h
@@ -24,12 +24,15 @@ typedef struct s_game
 		void	*zero;
 		void	*one;
 		void	*c;
-		void	*p;
-		void	*e;
+		void	*ded;
+		void	*pr;
+		void	*pl;
+		void	*pu;
+		void	*open;
+		void	*closed;
 	}t_img;
 	void	*wdw;
 	void	*mlx;
-	void	*img;
 	char	**matriz;
 	int		tall;
 	int		large;
@@ -42,6 +45,8 @@ typedef struct s_game
 	int		p_y;
 	int		pp_x;
 	int		pp_y;
+	int		end_x;
+	int		end_y;
 	int		i;
 	int		j;
 	int		w;
@@ -50,7 +55,7 @@ typedef struct s_game
 	int		d;
 }t_game;
 
-int		game_start(t_game *game, char *file);
+int		game_start(t_game *game, char *map, int fd);
 int		ft_strlen(const char *str);
 int		key_press(t_game *game);
 int		keydown(int key, t_game *game);
@@ -61,11 +66,14 @@ int		open_wdw(t_game *game);
 char	**map_cpy(int fd, char *argv, t_game	*game);
 int		wall_check(t_game *game);
 int		check_cpe(char **matriz, t_game *game);
+int		check_cpe_ifs(char **matriz, t_game *game);
 int		check_path(int i, int j, t_game *inf);
 int		check_path_check(char **matriz, t_game *game);
+void	food_collector(t_game *game);
 void	imgs_init(t_game *game);
 void	img_to_window(t_game *game);
 void	ifs(t_game *game);
 int		p_move(t_game *game);
+void	player_anim(t_game *game);
 
 #endif
