941ec21022de88bb6e40204af1dd5050ee9e14f1
Ekkoz897 <abelix897@gmail.com>
2023-01-18 04:27:36 +0000

    Organized files and functions2

--- so_long.h
+++ so_long.h
@@ -56,26 +56,33 @@ typedef struct s_game
 	int		d;
 }t_game;
 
-int		game_start(t_game *game);
+// Utils functions
 int		ft_strlen(const char *str);
-int		key_press(t_game *game);
-int		keydown(int key, t_game *game);
-int		keyup(int key, t_game *game);
-int		destroy_wdw(t_game *game);
 void	vars_init(t_game	*game);
+void	imgs_init(t_game *game);
+int		destroy_wdw(t_game *game);
 int		open_wdw(t_game *game);
+
+// Map validaters
 char	**map_cpy(int fd, char *argv, t_game	*game);
 int		wall_check(t_game *game);
 int		check_cpe(char **matriz, t_game *game);
 int		check_cpe_ifs(char **matriz, t_game *game);
 int		check_path(int i, int j, t_game *inf);
 int		check_path_check(char **matriz, t_game *game);
+
+// Animations display and key hooks
+int		game_start(t_game *game);
+int		key_press(t_game *game);
+int		keydown(int key, t_game *game);
+int		keyup(int key, t_game *game);
 void	food_collector(t_game *game);
-void	imgs_init(t_game *game);
 void	img_to_window(t_game *game);
 void	ifs(t_game *game);
 int		p_move(t_game *game);
 void	player_anim(t_game *game);
+
+// Post game display
 void	exit_check(t_game *game);
 void	close_first_window(t_game *game);
 void	display_victory(t_game game);
